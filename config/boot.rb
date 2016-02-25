6d812a43d84a46f89120ebc459522cff922f2aed
Jianping Zhou <jianping.zhou.1986@gmail.com>
2016-02-25 11:53:18 +0800

    set port to 5678 and set root path

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 5678)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
