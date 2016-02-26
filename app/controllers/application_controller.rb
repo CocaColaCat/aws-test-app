02cd197dfbdedbe9ecef43fb6330f28af79c3704
Jianping Zhou <jianping.zhou.1986@gmail.com>
2016-02-26 14:33:07 +0800

    set port to 5678 for production only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :use_port 
+  before_filter :use_port, if: ->{ Rails.env.production? }
+
   def use_port
     class << request
-        def port; 5678; end
+      def port; 5678; end
     end
   end
 
